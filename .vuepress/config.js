d6d5ddfacb41134106fa35288c8e27772cfc1a9a
JinMBP <sifnoc09@gmail.com>
2020-07-01 02:18:54 +0900

    docs: Setup vuepress with vercel

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,8 +1,42 @@
 module.exports = {
 	title: 'Sifnoc Blog',
-  description: 'blog powered by vuepress with vercel.',
-  themeConfig: {
-    nav: [{ text: 'Home', link: '/' }, { text: 'Docs', link: '/docs/' }]
-  },
-  dest: 'public'
+	description: 'blog powered by vuepress with vercel.',
+	theme: '@vuepress/theme-blog',
+	themeConfig: {
+		nav: [
+			{
+				text: 'Blog',
+				link: '/',
+			},
+			{
+				text: 'About',
+				link: '/about',
+			},
+		],
+		//footer: {
+		//	contact: [
+		//		{
+		//		type: 'github',
+		//		link: 'https://github.com/sifnoc',
+		//		},
+		//	]
+		//}
+	},
+	//patterns: ['docs/*.md', 'docs/vuepress/*.md'],
+	  plugins: [[
+			  '@vuepress/blog',
+		  {
+			  directories: [
+				  {
+					  id: 'post',
+					  dirname: 'docs',
+					  path: '/',
+					  pagination: {
+						  lengthPerPage: 10,
+					  },
+				  },
+			  ],
+		  },
+	  ],],
+	dest: 'public'
 }
