c44e9613dbc2f4dd914af986c28e19e54807dd65
JinMBP <sifnoc09@gmail.com>
2020-06-30 20:26:11 +0900

    Initialized for vuepress
    Updated test docs
    
    Updated build scripts
    
    Updated build scripts

---
+++ .vuepress/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+	title: 'Sifnoc Blog',
+  description: 'blog powered by vuepress with vercel.',
+  themeConfig: {
+    nav: [{ text: 'Home', link: '/' }, { text: 'Docs', link: '/docs/' }]
+  },
+  dest: 'public'
+}
